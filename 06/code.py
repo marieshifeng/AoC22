9123440a0757744359c7b4ba1b8756e737052545
Marie Shi Feng Saylor <marie@maries-mbp.lan>
2022-12-06 06:31:23 -0500

    day 6

---
+++ 06/code.py
@@ -0,0 +1,31 @@
+LEN_SOP = 4
+LEN_SOM = 14
+def process(filename, len_marker):
+    count = 0
+    cache = ""
+    with open(filename) as f:
+        while True:
+            c = f.read(1)
+            if not c:
+                break
+            if len(cache) < len_marker:
+                cache += c
+            else:
+                if len(set(cache)) == len_marker:
+                    return count
+                else:
+                    cache = cache[1:] + c
+            count += 1
+    f.close()
+    return count
+
+def part1(filename):
+   return process(filename, LEN_SOP)
+
+def part2(filename):
+   return process(filename, LEN_SOM)
+
+# filename = "small.txt"
+filename = "input.txt"
+print(part1(filename)) #1855
+print(part2(filename)) #3256
